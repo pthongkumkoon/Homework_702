06fa15d75fcf4232a78d681f5c9398c4fc39d656
pthongkumkoon <patcharawadee.t@cmu.ac.th>
2022-07-26 11:47:50 +0700

    Homework3_make it player

--- Homework_702.py
+++ Homework_702.py
@@ -5,6 +5,7 @@ Created on Mon Jul 25 17:22:25 2022
 Homework
 @author: p_thongkumkoon
 """
+import random
 
 name= input("What is your name?")
 Stick = int(input("How many stick in the pile?"))
@@ -22,6 +23,16 @@ while Stick > 0:
   else:
     Stick = Stick - A
     print ("There are", Stick, "in the pile.")
+    if Stick ==0:
+        print ("Game stop, you loose")
+        break
     time = time + 1
+    AI_pick = random.randint(1,2)
+    print ("AI_pick", AI_pick)
+    Stick = Stick - AI_pick
+    print ("There are", Stick, "in the pile.")
+    if Stick ==0:
+        print ("Game stop, you loose")
+        break
 print ("OK. There is no stick left in the pile. You spent", time, "times.")
     
